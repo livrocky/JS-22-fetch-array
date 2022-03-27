ce048585ab2b9e5ceaa89b8cb87a9b0fba1b8ec8
rokas <rokasra@gmail.com>
2022-03-27 18:02:03 +0300

    split name

--- js/cao.js
+++ js/cao.js
@@ -28,16 +28,19 @@ const tableHead1 = document.createElement("th");
 const tableHead2 = document.createElement("th");
 const tableHead3 = document.createElement("th");
 const tableHead4 = document.createElement("th");
+const tableHead5 = document.createElement("th");
 tableHead1.textContent = "Id";
 tableHead2.textContent = "Name";
-tableHead3.textContent = "City";
-tableHead4.textContent = "fav_color";
-tableRow.append(tableHead1, tableHead2, tableHead3, tableHead4);
+tableHead3.textContent = "Surname";
+tableHead4.textContent = "City";
+tableHead5.textContent = "fav_color";
+tableRow.append(tableHead1, tableHead2, tableHead3, tableHead4, tableHead5);
 function createSingleTable(userObject) {
   return `
     <table>
     <td>${userObject.id}</td>
-    <td>${userObject.name}</td>
+    <td>${userObject.name.split(" ")[0]}</td>
+    <td>${userObject.name.split(" ")[1]}</td>
     <td>${userObject.city}</td>
     <td>${userObject.fav_color}</td>
   </table>`;
