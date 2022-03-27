64f6b2f2ef3d3bb025a04e757b7c20a0764722a0
rokas <rokasra@gmail.com>
2022-03-27 17:26:18 +0300

    table with headers

--- js/cao.js
+++ js/cao.js
@@ -3,39 +3,42 @@
 //1. Naudojant tik JS, sukurkite lentelę ir į ją įrašykite duomenis (id, name, city, fav_color).
 
 //susikuriam lentele
-
-// const tableEl = document.createElement("table");
-// tableEl.className("table");
-// const mainTable = document.getElementById("mainTable");
-// const tableHeadEl = document.createElement("thead");
-// const tableBodyEl = document.createElement("tbody");
-
+const tableEl = document.createElement("table");
+tableEl.id = "mainTable";
+document.body.append(tableEl);
+console.log(tableEl);
 // parsisiusnciam products is API
 function getTable() {
   fetch("https://magnetic-melon-yam.glitch.me/")
     .then((resp) => resp.json())
     .then((data) => {
       console.log("data===", data);
-      data.forEach((uObj) => {
-        mainTable.insertAdjacentHTML("beforeend", createSingleTable(uObj));
+      data.forEach((userObject) => {
+        mainTable.insertAdjacentHTML("beforeend", createSingleTable(userObject));
       });
     })
     .catch((err) => console.log(err.message));
 }
 getTable();
 
+//susikuriam lentele html
+const tableRow = document.createElement("tr");
+tableEl.append(tableRow);
+const tableHead1 = document.createElement("th");
+const tableHead2 = document.createElement("th");
+const tableHead3 = document.createElement("th");
+const tableHead4 = document.createElement("th");
+tableHead1.textContent = "Id";
+tableHead2.textContent = "Name";
+tableHead3.textContent = "City";
+tableHead4.textContent = "fav_color";
+tableRow.append(tableHead1, tableHead2, tableHead3, tableHead4);
 function createSingleTable(userObject) {
   return `
-  <table>
-  <tr>
-      <th>Id</th>
-      <th>Name</th>
-      <th>City</th>
-      <th>fav_color</th>
-  </tr>
-  <td>${userObject.id}</td>
-  <td>${userObject.name}</td>
-  <td>${userObject.city}</td>
-  <td>${userObject.fav_color}</td>
-</table>`;
+    <table>
+    <td>${userObject.id}</td>
+    <td>${userObject.name}</td>
+    <td>${userObject.city}</td>
+    <td>${userObject.fav_color}</td>
+  </table>`;
 }
