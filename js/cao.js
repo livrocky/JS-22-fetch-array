3fb24985f204113a5be255fc0686b4fbe7a0c3cb
rokas <rokasra@gmail.com>
2022-03-28 20:29:50 +0300

    cao vip done

--- js/cao.js
+++ js/cao.js
@@ -8,38 +8,46 @@ tableEl.id = "mainTable";
 document.body.append(tableEl);
 // console.log(tableEl);
 // parsisiusnciam products is API
-function getTable() {
+function usersInfo(vip) {
   fetch("https://magnetic-melon-yam.glitch.me/")
     .then((resp) => resp.json())
     .then((data) => {
-      console.log("data===", data);
-      data.forEach((userObject) => {
-        mainTable.insertAdjacentHTML("beforeend", createSingleTable(userObject));
-      });
+      mainTable.innerHTML = "";
+      if (vip === true) {
+        const vip = data.filter((a) => a.vip);
+        vip.forEach((b) => {
+          mainTable.insertAdjacentHTML("beforeend", createSingleTable(b));
+        });
+      } else {
+        data.forEach((uObj) => {
+          mainTable.insertAdjacentHTML("beforeend", createSingleTable(uObj));
+        });
+      }
     })
     .catch((err) => console.log(err.message));
 }
-getTable();
+usersInfo();
 
 //susikuriam lentele html
-const tableRow = document.createElement("tr");
-tableEl.append(tableRow);
-const tableHead1 = document.createElement("th");
-const tableHead2 = document.createElement("th");
-const tableHead3 = document.createElement("th");
-const tableHead4 = document.createElement("th");
-const tableHead5 = document.createElement("th");
-const tableHead6 = document.createElement("th");
-tableHead1.textContent = "Id";
-tableHead2.textContent = "Image";
-tableHead3.textContent = "Name";
-tableHead4.textContent = "Surname";
-tableHead5.textContent = "City";
-tableHead6.textContent = "fav_color";
+// const tableRow = document.createElement("tr");
+// tableEl.append(tableRow);
+// const tableHead1 = document.createElement("th");
+// const tableHead2 = document.createElement("th");
+// const tableHead3 = document.createElement("th");
+// const tableHead4 = document.createElement("th");
+// const tableHead5 = document.createElement("th");
+// const tableHead6 = document.createElement("th");
+// tableHead1.textContent = "Id";
+// tableHead2.textContent = "Image";
+// tableHead3.textContent = "Name";
+// tableHead4.textContent = "Surname";
+// tableHead5.textContent = "City";
+// tableHead6.textContent = "fav_color";
 
-tableRow.append(tableHead1, tableHead2, tableHead3, tableHead4, tableHead5, tableHead6);
+// tableRow.append(tableHead1, tableHead2, tableHead3, tableHead4, tableHead5, tableHead6);
 
 //susikuriam checkbox html
+
 const checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 checkbox.name = "vip";
@@ -50,13 +58,14 @@ label.textContent = "VIP";
 document.body.prepend(checkbox);
 document.body.prepend(label);
 
-checkbox.addEventListener("change", function (userObject) {
-  if (this.checked) {
-    console.log("checkbox is checked");
+checkbox.addEventListener("input", (event) => {
+  if (event.target.checked) {
+    usersInfo(true);
   } else {
-    console.log("checkbox is unchecked");
+    usersInfo(false);
   }
 });
+
 //ikeliam
 function createSingleTable(userObject) {
   return `
